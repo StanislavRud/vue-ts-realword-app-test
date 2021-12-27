2170b7ab6881b2b2ab14685494f0cbae366a053d
Stanislav <stanislav.rud@itsvit.com>
2021-12-27 10:16:49 +0200

    Edited conduit to Kostya

--- src/components/HomeBanner.vue
+++ src/components/HomeBanner.vue
@@ -1,7 +1,7 @@
 <template functional>
   <div class="banner">
     <div class="container">
-      <h1 class="logo-font">conduit</h1>
+      <h1 class="logo-font">Hello Kostya</h1>
       <p>A place to share your knowledge.</p>
     </div>
   </div>
