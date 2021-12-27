3d614ecea9644b4b5b8e4ee457961538b93c2d09
Stanislav <stanislav.rud@itsvit.com>
2021-12-27 16:00:15 +0200

    added img

--- src/components/HomeBanner.vue
+++ src/components/HomeBanner.vue
@@ -2,6 +2,7 @@
   <div class="banner">
     <div class="container">
       <h1 class="logo-font">Hello Kostya :) </h1>
+      <img src="Kostya.png">
       <p>A place to share your knowledge.</p>
     </div>
   </div>
